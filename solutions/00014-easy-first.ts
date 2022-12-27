38f62920e9f3bc7d256c4812b8961af234667b16
RookieAND <gwangin1999@naver.com>
2022-12-27 23:19:53 +0900

    feat: 14번 문제의 다른 풀이 추가

--- solutions/00014-easy-first.ts
+++ solutions/00014-easy-first.ts
@@ -1,10 +1,16 @@
 /**
  * 00014 - Solution
  *
- * 빈 배열인 경우 never, 아닐 경우 배열의 0번째 인덱스를 지정한다.
+ * 첫 번째 방법 (easy)
+ * 단순하게 현재 T가 빈 배열인 경우 never, 아닐 경우 배열의 0번째 인덱스를 지정한다.
+ *
+ * 두 번째 방법 (normal)
+ * 만약 해당 튜플이 첫번째 원소 (U) 에 대한 타입 추론이 가능하다면 0번째 인덱스 값을 반환.
+ * 타입 추론에 실패할 경우는 오직 빈 배열이 들어왔을 경우이므로, never를 반환해야 함.
  */
 
 type First<T extends readonly unknown[]> = T extends [] ? never : T[0];
+type First2<T extends readonly unknown[]> = T extends readonly [infer U, ...any] ? U : never;
 
 /**
  *  Test Case
@@ -16,7 +22,11 @@ type cases = [
     Expect<Equal<First<[3, 2, 1]>, 3>>,
     Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
     Expect<Equal<First<[]>, never>>,
-    Expect<Equal<First<[undefined]>, undefined>>
+    Expect<Equal<First<[undefined]>, undefined>>,
+    Expect<Equal<First2<[3, 2, 1]>, 3>>,
+    Expect<Equal<First2<[() => 123, { a: string }]>, () => 123>>,
+    Expect<Equal<First2<[]>, never>>,
+    Expect<Equal<First2<[undefined]>, undefined>>
 ];
 
 type errors = [
